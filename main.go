93ca58f6bbf81c11b6b4c183099dd141ae4a59de
ajizblast <ajizblast@gmail.com>
2023-08-20 12:30:22 +0700

    hello-world-update

--- main.go
+++ main.go
@@ -1,11 +1,28 @@
+// package name
 package main
 
+// import package
 import (
 	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
+// Struct
+type Todos struct {
+	Id     string `json:"id"`
+	Title  string `json:"title"`
+	IsDone bool   `json:"isDone"`
+}
+
+//struct digunakan untuk mendefinisikan sebuah kata kunci ketika mau membuat object atau array of object
+//diatas ada sebuah struct bernama todos yang berisi Id bertipe data string, Tittle ini juga string, IsDone ini boolean
+//properti json yang dipakai nanti adalah yg berada di "", misal json:"id"
+
+// Dummy Data
+
+// diatas ada array of object dan berisi 2 object
 
 func main() {
 	e := echo.New()
