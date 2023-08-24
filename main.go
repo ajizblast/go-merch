293f400ebbde632f6030cc4d355a8e2f96eff5b8
ajizblast <ajizblast@gmail.com>
2023-08-24 09:56:54 +0700

    add routing

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 
 // import package
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -21,19 +22,128 @@ type Todos struct {
 //properti json yang dipakai nanti adalah yg berada di "", misal json:"id"
 
 // Dummy Data
+var todos = []Todos{
+	{
+		Id:     "1",
+		Title:  "Front End Engineer",
+		IsDone: true,
+	},
+	{
+		Id:     "2",
+		Title:  "Mobile Dev Engineer",
+		IsDone: false,
+	},
+	{
+		Id:     "3",
+		Title:  "Java Developer",
+		IsDone: false,
+	},
+}
 
+// json encode and decode
 // diatas ada array of object dan berisi 2 object
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		// c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-		c.Response().Header().Set("Content-Type", "application/json")
-		c.Response().WriteHeader(http.StatusOK)
-		return c.String(http.StatusOK, "Hello World!")
-	})
+	e.GET("/", FindTodos)
+	e.GET("/todo/:id", GetTodo)
+	e.POST("/todo", CreateTodo)
+	e.PATCH(("/todo/:id"), UpdateTodo)
+	e.DELETE("/todo/:id", DeleteTodo)
 
 	fmt.Println("Server running on localhost:5000")
 	e.Logger.Fatal(e.Start("localhost:5000"))
+}
+
+// Get all Todo data
+func FindTodos(c echo.Context) error {
+	c.Response().Header().Set("Content-Type", "application/json")
+	c.Response().WriteHeader(http.StatusOK)
+
+	return json.NewEncoder(c.Response()).Encode(todos)
+}
+
+func GetTodo(c echo.Context) error {
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+	id := c.Param("id")
+
+	var todoData Todos
+	var isGetTodo = false
+
+	for _, todo := range todos {
+		if id == todo.Id {
+			isGetTodo = true
+			todoData = todo
+		}
+	}
+
+	if !isGetTodo {
+		c.Response().WriteHeader(http.StatusNotFound)
+		return json.NewEncoder(c.Response()).Encode("ID: " + id + " not found")
+	}
+
+	c.Response().WriteHeader(200)
+	return json.NewEncoder(c.Response()).Encode(todoData)
+}
+
+func CreateTodo(c echo.Context) error {
+	var data Todos
+
+	json.NewDecoder(c.Request().Body).Decode(&data)
+
+	todos = append(todos, data)
+
+	c.Response().Header().Set("Content-Type", "application/json")
+	c.Response().WriteHeader(http.StatusOK)
+	return json.NewEncoder(c.Response()).Encode(todos)
+}
+
+func UpdateTodo(c echo.Context) error {
+	var id = c.Param("id")
+	var data Todos
+	isGetTodo := false
+
+	json.NewDecoder(c.Request().Body).Decode(&data)
+
+	for idx, todo := range todos {
+		if id == todo.Id {
+			isGetTodo = true
+			todos[idx] = data
+		}
+	}
+
+	if !isGetTodo {
+		c.Response().WriteHeader(http.StatusNotFound)
+		return json.NewEncoder(c.Response()).Encode("ID: " + id + " not found")
+	}
+
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	c.Response().WriteHeader(http.StatusOK)
+	return json.NewEncoder(c.Response()).Encode(data)
+}
+
+func DeleteTodo(c echo.Context) error {
+	id := c.Param("id")
+	var isGetTodo = false
+	var index = 0
+
+	for i, todo := range todos {
+		if id == todo.Id {
+			isGetTodo = true
+			index = i
+		}
+	}
+
+	if !isGetTodo {
+		c.Response().WriteHeader(400)
+		return json.NewEncoder(c.Response()).Encode("ID: " + id + " not found")
+	}
+
+	todos = append(todos[:index], todos[index+1:]...)
+
+	c.Response().Header().Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	c.Response().WriteHeader(http.StatusOK)
+	return json.NewEncoder(c.Response()).Encode(todos)
 }
